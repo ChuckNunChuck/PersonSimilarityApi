9af711a5aa97ad44c2fba6738b2f9dcb02964b5e
Dmytro Ivanchyshyn <dmytro.ivanchyshyn@carnext.com>
2021-11-21 14:47:45 +0100

    Add applications tests project

---
+++ FraudDetector.Application.Tests/Services/SimilarityCalculatorTests.cs
@@ -0,0 +1,31 @@
+using AutoFixture;
+using FluentAssertions;
+using FraudDetector.Application.Persons.Model;
+using FraudDetector.Application.Services;
+using FraudDetector.Domain.Model;
+using Xunit;
+
+namespace FraudDetector.Application.Tests.Services;
+
+public class SimilarityCalculatorTests
+{
+    private readonly Fixture _fixture;
+
+    public SimilarityCalculatorTests()
+    {
+        _fixture = new Fixture();
+    }
+
+    [Fact]
+    public void CalculatesSimilarity()
+    {
+        var similarityCalculator = _fixture.Create<SimilarityCalculator>();
+        var person = _fixture.Create<Person>();
+
+        var result = similarityCalculator.Calculate(
+            person, 
+            new SimilarPerson(person.FirstName, person.LastName, person.DateOfBirth, person.IdentificationNumber));
+
+        result.Should().Be(1m);
+    }
+}
