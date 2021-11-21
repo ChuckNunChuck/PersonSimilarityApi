e59211d21bcc4750cb2400692e832ef6b2f739f2
Dmytro Ivanchyshyn <dmytro.ivanchyshyn@carnext.com>
2021-11-21 17:31:09 +0100

    Update rules

--- src/FraudDetector.Application/Services/SimilarityCalculator.cs
+++ src/FraudDetector.Application/Services/SimilarityCalculator.cs
@@ -19,23 +19,44 @@ public class SimilarityCalculator : ISimilarityCalculator
             ? PositiveResult
             : CalculateBasedOnNameAndBirthDate(person, similarPerson);
 
-    private decimal CalculateBasedOnNameAndBirthDate(Person person, SimilarPerson similarPerson)
+    private static decimal CalculateBasedOnNameAndBirthDate(Person person, SimilarPerson similarPerson)
     {
         if (!HasSameDateOfBirth(person, similarPerson))
             return NegativeResult;
 
         var notOccurProbability = PositiveResult;
-        if (person.LastName.Equals(similarPerson.LastName, StringComparison.InvariantCulture))
-            notOccurProbability *= SameLastNameProbability.InvertProbability();
 
-        if (person.FirstName.Equals(similarPerson.FirstName, StringComparison.InvariantCulture))
-            notOccurProbability *= SameFirstNameProbability.InvertProbability();
-
-        if (HasSameDateOfBirth(person, similarPerson))
-            notOccurProbability *= SameDateOfBirthProbability.InvertProbability();
+        notOccurProbability *= FirstNameNotOccurProbability(person, similarPerson);
+        notOccurProbability *= LastNameNotOccurProbability(person, similarPerson);
+        notOccurProbability *= DateOfBirthNotOccurProbability(person, similarPerson);
 
         return notOccurProbability.InvertProbability();
+    }
+    private static decimal FirstNameNotOccurProbability(Person person, SimilarPerson similarPerson)
+    {
+        if (person.FirstName.Equals(similarPerson.FirstName, StringComparison.InvariantCulture))
+        {
+            return SameFirstNameProbability.InvertProbability();
+        }
 
+        return HasSimilarFirstName(person.FirstName, similarPerson.FirstName)
+            ? SimilarFirstNameProbability.InvertProbability()
+            : PositiveResult;
+    }
+
+    private static decimal LastNameNotOccurProbability(Person person, SimilarPerson similarPerson) =>
+        person.LastName.Equals(similarPerson.LastName, StringComparison.InvariantCulture)
+            ? SameLastNameProbability.InvertProbability()
+            : PositiveResult;
+
+    private static decimal DateOfBirthNotOccurProbability(Person person, SimilarPerson similarPerson) =>
+        HasSameDateOfBirth(person, similarPerson) 
+            ? SameDateOfBirthProbability.InvertProbability() 
+            : PositiveResult;
+
+    private static bool HasSimilarFirstName(string personFirstName, string similarPersonFirstName)
+    {
+        throw new NotImplementedException();
     }
 
     private static bool HasSameDateOfBirth(Person person, SimilarPerson similarPerson) =>
